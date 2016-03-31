60f7eb1e7af64d404313c9dfb37360a1ed5b5c5c
Lawrence Thorpe <techssh@gmail.com>
2016-03-31 19:48:31 +0100

    -

---
+++ SortPixels/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.sortButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(901, 465);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // sortButton
+            // 
+            this.sortButton.Enabled = false;
+            this.sortButton.Location = new System.Drawing.Point(85, 3);
+            this.sortButton.Name = "sortButton";
+            this.sortButton.Size = new System.Drawing.Size(75, 42);
+            this.sortButton.TabIndex = 1;
+            this.sortButton.Text = "Sort";
+            this.sortButton.UseVisualStyleBackColor = true;
+            this.sortButton.Click += new System.EventHandler(this.sortButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(4, 3);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 42);
+            this.loadButton.TabIndex = 3;
+            this.loadButton.Text = "Browse..";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Edge Detection Threshold";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 3;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Location = new System.Drawing.Point(169, 24);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(128, 20);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            65536});
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(3, 520);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(895, 18);
+            this.progressBar.TabIndex = 9;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.Enabled = false;
+            this.saveButton.Location = new System.Drawing.Point(823, 3);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 42);
+            this.saveButton.TabIndex = 10;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(303, 5);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(72, 17);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Horizontal";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(303, 28);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(60, 17);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.Text = "Vertical";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 541);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.sortButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "SortPixels";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button sortButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ SortPixels/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void sortButton_Click(object sender, EventArgs e)
+        {
+            float edgeThreshold = (float)numericUpDown1.Value;
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            List<List<Color>> sortedPixels = new List<List<Color>>();
+            if (radioButton2.Checked) image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            progressBar.Maximum = image.Height;
+            Cursor = Cursors.WaitCursor;
+            for (int y = 0; y < image.Height; y++)
+            {
+                Dictionary<int, Color> row = new Dictionary<int, Color>();
+                Dictionary<int, Color> rowPart = new Dictionary<int, Color>();
+                for (int x = 0; x < image.Width; x++)
+                {
+                    if (x > 0)
+                    {
+                        float leftPixelBrightness = image.GetPixel(x - 1, y).GetBrightness();
+                        if (leftPixelBrightness - edgeThreshold > image.GetPixel(x, y).GetBrightness() || leftPixelBrightness + edgeThreshold < image.GetPixel(x, y).GetBrightness())
+                        {
+                            rowPart.Add(x, image.GetPixel(x, y));
+                        }
+                        else
+                        {
+                            row.Add(x, image.GetPixel(x, y));
+                        }
+                    }
+                    else
+                    {
+                        row.Add(x, image.GetPixel(x, y));
+                    }
+                    progressBar.Value = y;
+                }
+                List<Color> rowPartValList = rowPart.Values.ToList();
+                List<int> rowPartKeyList = rowPart.Keys.ToList();
+                rowPartValList.Sort(delegate (Color left, Color right)
+                {
+                    return left.GetBrightness().CompareTo(right.GetBrightness());
+                });
+                for (int p = 0; p < rowPartValList.Count; p++)
+                {
+                    row.Add(rowPartKeyList[p], rowPartValList[p]);
+                }
+                sortedPixels.Add(row.Values.ToList());
+            }
+
+            Bitmap imageSorted = image;
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    imageSorted.SetPixel(x, y, sortedPixels[y][x]);
+                }
+            }
+            progressBar.Value = image.Height;
+            if (radioButton2.Checked) imageSorted.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            pictureBox1.Image = imageSorted;
+            Cursor = Cursors.Default;
+            saveButton.Enabled = true;
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            o.Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+            o.ShowDialog();
+            try
+            {
+                pictureBox1.Image = new Bitmap(Image.FromFile(o.FileName));
+                sortButton.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.FileName = "Sorted.jpg";
+            s.DefaultExt = "jpg";
+            s.Filter = "Image file (*.jpg) | *.jpg";
+            s.ShowDialog();
+            try
+            {
+                pictureBox1.Image.Save(s.FileName);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
